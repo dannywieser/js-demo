623fa9b69b5af5e34167f9c66e80fe0f19ce21cc
dannywieser <danny.wieser@blackboard.com>
2018-11-02 14:45:15 -0600

    yet more debugging

--- packages/redux-example/_demo/webpack.config.js
+++ packages/redux-example/_demo/webpack.config.js
@@ -1,2 +1,3 @@
 const outpath = __dirname + '/../../../gh-pages/js-demo-redux-example';
+console.log(outpath);
 module.exports = require('@js-demo/core/webpack/webpack').demoConfig('_demo', outpath);
